6a394d362432326c237c5451667338eafa4fd412
Kamila Łopatowska <146941710+KamilaLopatowska@users.noreply.github.com>
2024-04-28 17:21:57 +0200

    Add files via upload

---
+++ lalala.py
@@ -0,0 +1,53 @@
+from tabulate import tabulate
+
+
+#ilość produktów potrzebnych na wskazany dzień
+ilosc = int(input("Podaj ilość gier, które potrzebujesz:"))
+dzien = input("Podaj nr tygodnia (1-7), na który je potrzebujesz: ")
+
+#zależność w ilości między produktami różnych poziomów
+
+#database
+gra = {"tydzień": ["potrzeby brutto", "wstępny zapas", "potrzeby netto", "wstępne zmontowanie", "zaplanowany odbiór"],
+        "1": ["","2","","",""],
+        "2": ["","2","","",""],
+        "3": ["","2","","",""],
+        "4": ["","2","","18",""],
+        "5": ["20","2","18","","18"],
+        "6":["","","","40",""],
+        "7":["40","","40","","40"]}
+gra[dzien][0] = ilosc
+opakowanie = {"tydzień": ["potrzeby brutto", "wstępny zapas", "potrzeby netto", "wstępne zmontowanie", "zaplanowany odbiór"],
+        "1": ["","2","","",""],
+        "2": ["","2","","",""],
+        "3": ["","2","","",""],
+        "4": ["","2","","18",""],
+        "5": ["20","2","18","","18"],
+        "6":["","","","40",""],
+        "7":["40","","40","","40"]}
+karty = {"tydzień": ["potrzeby brutto", "wstępny zapas", "potrzeby netto", "wstępne zmontowanie", "zaplanowany odbiór"],
+        "1": ["","2","","",""],
+        "2": ["","2","","",""],
+        "3": ["","2","","",""],
+        "4": ["","2","","18",""],
+        "5": ["20","2","18","","18"],
+        "6":["","","","40",""],
+        "7":["40","","40","","40"]}
+karton = {"tydzień": ["potrzeby brutto", "wstępny zapas", "potrzeby netto", "wstępne zmontowanie", "zaplanowany odbiór"],
+        "1": ["","2","","",""],
+        "2": ["","2","","",""],
+        "3": ["","2","","",""],
+        "4": ["","2","","18",""],
+        "5": ["20","2","18","","18"],
+        "6":["","","","40",""],
+        "7":["40","","40","","40"]}
+plastik = {"tydzień": ["potrzeby brutto", "wstępny zapas", "potrzeby netto", "wstępne zmontowanie", "zaplanowany odbiór"],
+        "1": ["","2","","",""],
+        "2": ["","2","","",""],
+        "3": ["","2","","",""],
+        "4": ["","2","","18",""],
+        "5": ["20","2","18","","18"],
+        "6":["","","","40",""],
+        "7":["40","","40","","40"]}
+df = tabulate(gra, headers = "keys")
+print(df)
