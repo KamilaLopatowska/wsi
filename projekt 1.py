97b74e32306630fbe346461c16df26cdd2a5d815
Małgorzata Łabuzek <146942844+mallab20@users.noreply.github.com>
2024-04-28 17:23:13 +0200

    Add files via upload

---
+++ projekt 1.py
@@ -0,0 +1,16 @@
+produkt = 'gra'
+elementy = ['20 kart', 'pudełko']
+
+#określenie startowej dostępności
+#poziom 0
+dostepnosc_gra = int(input("Początkowa dostępność gry (w sztukach): "))
+#poziom1
+dostepnosc_opakowanie = int(input("Początkowa dostępność opakowań (w sztukach): "))
+dostepnosc_zestaw_kart = int(input("Początkowa dostępność zestawów kart (w sztukach): "))
+#poziom2
+dostepnosc_karton = int(input("Początkowa dostępność kartonów (w sztukach): "))
+dostepnosc_plastik = int(input("Początkowa dostępność plastikowych części (w sztukach): "))
+
+#input klienta
+ilosc = int(input('Ile sztuk gry: '))
+czas = int(input('Na kiedy: '))
